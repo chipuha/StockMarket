89b0ae4bd055a8863b19e29581a6fede472aecd9
AJ <rasmussonaxj@gmail.com>
2018-09-28 23:44:05 -0400

    updated model with a stock and stockmodel class; not totally set on table relationships...

--- Visualizer/stockadvisor/advisor/admin.py
+++ Visualizer/stockadvisor/advisor/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Stock, StockModel
+
+admin.site.register(Stock)
+admin.site.register(StockModel)
--- Visualizer/stockadvisor/advisor/models.py
+++ Visualizer/stockadvisor/advisor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model):
+    name = models.CharField(max_length=200)
+    ticker = models.CharField(max_length=20)
+    sell_signal = models.DecimalField(default=0.00,max_digits=5,decimal_places=2)
+    buy_signal = models.DecimalField(default=0.00,max_digits=5,decimal_places=2)
+    hold_signal = models.DecimalField(default=0.00,max_digits=5,decimal_places=2)
+    time_series_prices = models.FloatField(default=0.0)
+
+class StockModel(models.Model):
+    name = models.CharField(max_length=200)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    time_series_plot = models.FloatField(default=0.0)
