e2ab4a37a1ef8346b1d090369078f11ee7954211
AJ <rasmussonaxj@gmail.com>
2018-11-23 12:02:01 -0600

    almost finished with update button--not finished yet

--- Visualizer/stock-advisor/Data/GetStockData.py
+++ Visualizer/stock-advisor/Data/GetStockData.py
@@ -8,61 +8,66 @@ from datetime import datetime, timedelta, date
 
 import requests
 import pandas as pd
+import pandas_datareader as pdr
 
 #gets data for short (a few months back) time periods
 class PandasDataReaderHistory:
     
+    # Import stock data using pandasreader
+    def getData(self, startDate, endDate):
+        df = pdr.get_data_quandl(self.ticker, start=startDate, end=endDate)
+        df = df.reindex(index=df.index[::-1]) #flip index so 0th row is oldest date
+        return df
+    
     def __init__(self, ticker): #executed when an sma object in created
         self.ticker = str(ticker)   #must be all caps string (e.g. "MSFT")
-        outputfilename='CSVs/'+self.ticker+'.csv'
+        outputfilename='Data/CSVs/'+self.ticker+'.csv'
         
         total = 0 #track total number of tickers processed
         alreadyUpToDate = [] #track how many files were already up to date
         updated = [] #track how many files were updated
         errored = [] #track which stocks were has errors
-        try:
-            print('CSVs/'+str(self.ticker)+'.csv')
-            df = pd.read_csv('CSVs/'+str(self.ticker)+'.csv')
-            print('in')
-            #update file if it exists
-            #check if the df's latest date is today's date, otherwise update
-            if df['Date'].tail(1).item()==date.today().__str__():
-                print('here3')
-                #print('already up to date') #do nothing
+        #try:
+        print('Data/CSVs/'+str(self.ticker)+'.csv')
+        df = pd.read_csv('Data/CSVs/'+str(self.ticker)+'.csv')
+        print('in')
+        #update file if it exists
+        #check if the df's latest date is today's date, otherwise update
+        if df['Date'].tail(1).item()==date.today().__str__():
+            print('here3')
+            #print('already up to date') #do nothing
+            alreadyUpToDate.append(self.ticker)
+        else:
+            print('here2')
+            print(str(df['Date'].tail(1).item()))
+            days_back = (date.today()-datetime.strptime(df['Date'].tail(1).item(),"%Y-%m-%d").date()).days
+            #check if you have Friday's data and it's Saturday/Sunday 
+            if days_back < 3 and date.today().weekday() > 4:
                 alreadyUpToDate.append(self.ticker)
             else:
-                print('here2')
-                days_back = (date.today()-datetime.strptime(df['Date'].tail(1).item(),"%Y-%m-%d").date()).days
-                #check if you have Friday's data and it's Saturday/Sunday 
-                if days_back < 3 and date.today().weekday() > 4:
-                    alreadyUpToDate.append(self.ticker)
-                else:
-                    df2 = getData(self.ticker,df['Date'].tail(1).item(),date.today())
-                    df = df.append(df2)
-                    df.to_csv(path_or_buf=outputfilename)
-                    updated.append(self.ticker)
-        except: #if file doesn't exist, make it
-            try:
-                print('here1')
-                #get last year of data
-                df = getData(self.ticker,date.today() - timedelta(365),date.today())
-                df.to_csv(path_or_buf=outputfilename)
+                df2 = pdr.get_data_quandl(self.ticker, start=df['Date'].tail(1).item(), end=date.today())
+                print('head\n',df2.head())
+                df2 = df2.reindex(index=df.index[::-1])
+                df = pd.concat([df, df2])
+                #df.to_csv(path_or_buf=outputfilename)
                 updated.append(self.ticker)
-            except: 
-                print('here')
-                errored.append(ticker)
+        #except as e: #if file doesn't exist, make it
+        #print(e) 
+        try:
+            print('here1')
+            #get last year of data
+            df = getData(self.ticker,date.today() - timedelta(365),date.today())
+            df.to_csv(path_or_buf=outputfilename)
+            updated.append(self.ticker)
+        except: 
+            print('here')
+            errored.append(ticker)
                 
         #print download diagnostics
         print(str(len(alreadyUpToDate))+' out of '+str(total)+' were already up-to-date')
         print(str(len(updated))+' out of '+str(total)+' were updated')
         if len(errored) > 0:
             print(str(errored)+' errored')
-        
-    # Import stock data using pandasreader
-    def getData(self, startDate, endDate):
-        df = pdr.get_data_quandl(self.ticker, start=startDate, end=endDate)
-        df = df.reindex(index=df.index[::-1]) #flip index so 0th row is oldest date
-        return df
             
     
 #gets data for long time periods
